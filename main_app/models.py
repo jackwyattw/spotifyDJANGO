0a192b03ec65e1b48aab3129b8c61b1bf9a0a857
Jack W <wikum.wyatt@gmail.com>
2022-10-06 13:29:56 -0500

    sneakers collector

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Artist(models.Model):
 
     name = models.CharField(max_length=100)
-    img = models.CharField(max_length=250)
-    bio = models.TextField(max_length= 300)
+    img = models.CharField(max_length=500)
+    bio = models.TextField(max_length= 1000)
     verified_artist = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     
