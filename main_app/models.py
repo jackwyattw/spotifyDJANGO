e885cf44374d6243483591b4e5a1d6262d70c421
Jack W <wikum.wyatt@gmail.com>
2022-10-06 10:58:35 -0500

    first commit

---
+++ main_app/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+
+class Artist(models.Model):
+
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    bio = models.TextField(max_length= 300)
+    verified_artist = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
---
+++ main_app/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse # <- a class to handle sending a type of response
+from django.views.generic.base import TemplateView
+
+
+# Create your views here.
+
+# Here we will be creating a class called Home and extending it from the View class
+class Home(TemplateView):
+    template_name = 'home.html'
+
+class About(TemplateView):
+    template_name = 'about.html'
+
+
+class Artist:
+    def __init__(self, name, image, bio):
+        self.name = name
+        self.image = image
+        self.bio = bio
+
+
+artists = [
+  Artist("Gorillaz", "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29",
+          "Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around."),
+  Artist("Panic! At The Disco",
+          "https://i.scdn.co/image/58518a04cdd1f20a24cf0545838f3a7b775f8080", "Welcome 👋 The Amazing Beebo was working on a new bio but now he's too busy taking over the world."),
+  Artist("50 Cent", "https://townsquare.media/site/812/files/2022/09/attachment-50.jpg?w=980&q=75",
+          "50 is the most gangsta rapper there is"),
+  Artist("Metallica",
+          "https://i.scdn.co/image/ab67706c0000da84eb6bb372a485d26fd32d1922", "Metallica formed in 1981 by drummer Lars Ulrich and guitarist and vocalist James Hetfield and has become one of the most influential and commercially successful rock bands in history, having sold 110 million albums worldwide while playing to millions of fans on literally all seven continents."),
+  Artist("Juice Wrld",
+          "https://imagez.tmz.com/image/e7/4by3/2019/12/08/e7d6d436f2794f6cbe077473f7fbf21e_md.jpg", "Jarad Anthony Higgins, forever in our hearts. LND999"),
+  Artist("Kaskade",
+          "https://i1.sndcdn.com/artworks-sNjd3toBZYCG-0-t500x500.jpg", "Ryan Gary Raddon, better known by his stage name Kaskade, is an American DJ, record producer, and remixer."),
+]
+
+class ArtistList(TemplateView):
+    template_name = "artist_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["artists"] = artists # this is where we add the key into our context object for the view to use
+        return context
